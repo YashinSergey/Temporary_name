96e43d34b55d1a472368226a2bd5b62b23233433
mr.Blue <yashinsergey125@gmail.com>
2019-12-06 17:55:06 +0300

    added closeDrawer to onBackPressed

--- app/src/main/java/com/example/temporaryname/MainActivity.java
+++ app/src/main/java/com/example/temporaryname/MainActivity.java
@@ -89,4 +89,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
