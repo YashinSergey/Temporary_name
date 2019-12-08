d0a85d30be40ff0d1209f786f5bc1fa2dcdf6a77
mr.Blue <yashinsergey125@gmail.com>
2019-12-08 18:45:06 +0300

    modified fragment

--- app/src/main/java/com/example/temporaryname/ui/fragments/MuscleCarsFragment.java
+++ app/src/main/java/com/example/temporaryname/ui/fragments/MuscleCarsFragment.java
@@ -6,13 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable;
 
 import com.example.temporaryname.R;
 
-public class MuscleCarsFragment extends Fragment {
+public class MuscleCarsFragment extends BaseFragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_muscle_cars, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.muscle_car));
+        textViewTitle.setText(R.string.muscle_cars_title);
+        textViewContent.setText(getString(R.string.about_muscle_cars1));
+    }
+
 }
